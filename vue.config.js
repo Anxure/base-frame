0eb9c7b29df895c4f63b4abc2955f12813f50946
Anxure <505959201@qq.com>
2021-01-12 09:54:13 +0800

    chore: update axios

--- vue.config.js
+++ vue.config.js
@@ -83,7 +83,7 @@ module.exports = {
             chunks: 'initial' // only package third parties that are initially dependent
           },
           elementUI: {
-            name: 'chunk-viewDesign', // split elementUI into a single package
+            name: 'chunk-viewDesign', // split viewDesign into a single package
             priority: 20, // the weight needs to be larger than libs and app or it will be packaged into libs or app
             test: /[\\/]node_modules[\\/]_?view-design(.*)/ // in order to adapt to cnpm
           },
