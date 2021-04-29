ae9d7ab9635414ffbf6aca04e48d0a3a80297f0d
Anxure <505959201@qq.com>
2021-04-29 11:47:39 +0800

    other:删除多余的文件

--- vue.config.js
+++ vue.config.js
@@ -82,7 +82,7 @@ module.exports = {
             priority: 10,
             chunks: 'initial' // only package third parties that are initially dependent
           },
-          elementUI: {
+          ViewDesign: {
             name: 'chunk-viewDesign', // split viewDesign into a single package
             priority: 20, // the weight needs to be larger than libs and app or it will be packaged into libs or app
             test: /[\\/]node_modules[\\/]_?view-design(.*)/ // in order to adapt to cnpm
